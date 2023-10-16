7b0538a9d390aeb5528a2f7a214e2871b2bfc7fa
Brandon <93497583+hebrd@users.noreply.github.com>
2023-10-16 22:48:05 +0800

    pesistent

---
+++ src/main/java/com/polarbookshop/catalogservice/config/DataConfig.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.catalogservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
+
+/**
+ * @author brandon
+ * @date 16/10/2023
+ */
+@Configuration
+@EnableJdbcAuditing
+public class DataConfig {
+}
--- src/main/java/com/polarbookshop/catalogservice/domain/BookRepository.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/BookRepository.java
@@ -1,20 +1,23 @@
 package com.polarbookshop.catalogservice.domain;
 
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.Optional;
 
 /**
  * @author brandon
  * @date 15/10/2023
  */
-public interface BookRepository {
+public interface BookRepository extends CrudRepository<Book, Long> {
 
-    Iterable<Book> findAll();
+  Optional<Book> findByIsbn(String isbn);
+  boolean existsByIsbn(String isbn);
 
-    Optional<Book> findByIsbn(String isbn);
-
-    boolean existsByIsbn(String isbn);
-
-    Book save(Book book);
-
-    void deleteByIsbn(String isbn);
+  @Modifying
+  @Transactional
+  @Query("delete from Book where isbn = :isbn")
+  void deleteByIsbn(String isbn);
 }
